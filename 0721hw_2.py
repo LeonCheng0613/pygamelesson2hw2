9d13fd1914eaf6a655e02f8759bb6619192ffcec
LeonCheng0613 <86943239+LeonCheng0613@users.noreply.github.com>
2021-07-22 21:23:52 +0800

    Add files via upload

---
+++ 0721hw_2.py
@@ -0,0 +1,12 @@
+list1 = ['M', 'na', 'i', 'ke']
+list2 = ['y', 'me', 's', 'lly']
+list3 = []
+a = list1[0] + list2[0]
+list3.append(a)
+a1 = list1[1] + list2[1]
+list3.append(a1)
+a2 = list1[2] + list2[2]
+list3.append(a2)
+a3 = list1[3] + list2[3]
+list3.append(a3)
+print(list3)
